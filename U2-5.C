29c268a4e8ae8e5457ea43febffadf9cc35f5823
preetkeraliya-glitch <preetkeraliya@gmail.com>
2026-07-31 10:44:10 +0530

    Add files via upload

---
+++ U2-5.C
@@ -0,0 +1,26 @@
+//wap to find area of rectangle
+
+#include<stdio.h>
+#include<conio.h>
+
+void main ()
+
+{
+
+	int l,b,a;
+
+	clrscr();
+
+	printf("\n Enter velue of l :");
+	scanf("%d",&l);
+
+	printf("\n Enter velue of b :");
+	scanf("%d",&b);
+
+	a=l*b;
+
+	printf("\n Area of Rectangle %d",a);
+
+	getch();
+
+}
---
+++ U2-6.C
@@ -0,0 +1,23 @@
+//wap to find 3 number avg
+
+#include<stdio.h>
+#include<conio.h>
+
+void main ()
+
+{
+
+	int x,y,z,avg;
+
+	clrscr();
+
+	printf("\n Enter velue of x , y , z :");
+	scanf("%d%d%d",&x,&y,&z);
+
+       //total = x + y + z;
+	avg = (x+y+z) / 3;
+	printf("\n Average is : %d",avg);
+
+	getch();
+
+}
