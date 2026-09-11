2516d4f3dd008ce32ab0159509f8f45479ba70cb
preetkeraliya-glitch <preetkeraliya@gmail.com>
2026-09-11 11:04:00 +0530

    Add files via upload

---
+++ DEFINE1.C
@@ -0,0 +1,16 @@
+// Defining Symbolic Constants
+
+#include<stdio.h>
+#include<conio.h>
+
+#define PI 3.14
+#define pf printf
+
+void main()
+{
+	float r=2.5 , a;
+	clrscr();
+	a = PI * r * r;
+	pf("\n Area is : %f ",a);
+	getch();
+}
